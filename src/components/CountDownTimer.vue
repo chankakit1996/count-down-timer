a525bfc82abb49aa8c340bd2d01249643a2bf471
Ken Chan <chankakit1996@yahoo.com.hk>
2022-02-02 20:32:50 +0800

    updated readme and after christmas UI

--- src/components/CountDownTimer.vue
+++ src/components/CountDownTimer.vue
@@ -1,53 +1,62 @@
 <template>
     <div class="container">
         <h1 class="title">This is a Ken Count Down Timer</h1>
-        <h2 class="timer-title">2022 Christmas Day</h2>
-        <div class="timer-wrapper">
-            <div class="date-wrapper day-wrapper">
-                <div class="date day">
-                    {{ time.days }}
+        <template v-if="time.total > 0">
+            <h2 class="timer-title">2022 Christmas Day</h2>
+            <div class="timer-wrapper">
+                <div class="date-wrapper day-wrapper">
+                    <div class="date day">
+                        {{ time.days }}
+                    </div>
+                    <div class="label">days</div>
                 </div>
-                <div class="label">days</div>
-            </div>
-            <div class="date-wrapper hour-wrapper">
-                <div class="date hour">
-                    {{ time.hours }}
+                <div class="date-wrapper hour-wrapper">
+                    <div class="date hour">
+                        {{ time.hours }}
+                    </div>
+                    <div class="label">hours</div>
                 </div>
-                <div class="label">hours</div>
+                <div class="date-wrapper minute-wrapper">
+                    <div class="date minute">{{ time.minutes }}</div>
+                    <div class="label">mins</div>
+                </div>
+                <div class="date-wrapper second-wrapper">
+                    <div class="date second">{{ time.seconds }}</div>
+                    <div class="label">seconds</div>
+                </div>
             </div>
-            <div class="date-wrapper minute-wrapper">
-                <div class="date minute">{{ time.minutes }}</div>
-                <div class="label">mins</div>
-            </div>
-            <div class="date-wrapper second-wrapper">
-                <div class="date second">{{ time.seconds }}</div>
-                <div class="label">seconds</div>
-            </div>
-        </div>
+        </template>
+        <template v-else>
+            <h2 class="timer-title">2022 Christmas Day is over</h2>
+            <sad-icon class="sad-icon-wrapper" />
+        </template>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import SadIcon from './SadIcon.vue';
 
 export default defineComponent({
+    components: { SadIcon },
     name: 'CountDownTimer',
     setup() {
         const getTime = () => {
             const christmasDate = new Date('2022-12-25');
             const now = new Date();
-            const result = (christmasDate.getTime() - now.getTime()) / 1000; // millisecond
+            const total = (christmasDate.getTime() - now.getTime()) / 1000; // millisecond
 
-            const seconds = Math.floor(result % 60);
-            const minutes = Math.floor(result / 60) % 60;
-            const hours = Math.floor(result / 60 / 60) % 24;
-            const days = Math.floor(result / 60 / 60 / 24);
+            const seconds = Math.floor(total % 60);
+            const minutes = Math.floor(total / 60) % 60;
+            const hours = Math.floor(total / 60 / 60) % 24;
+            const days = Math.floor(total / 60 / 60 / 24);
 
             return {
                 days,
                 hours,
                 minutes,
                 seconds,
+                total,
             };
         };
         const time = ref(getTime());
@@ -81,6 +90,11 @@ export default defineComponent({
                 font-size: 5rem;
                 font-weight: bold;
             }
+        }
+
+        > .sad-icon-wrapper {
+            width: 100px;
+            height: 100px;
         }
     }
 }
