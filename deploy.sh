ab86199ac6f2fe25236a66ced13ff8badddd4a8f
Ken Chan <chankakit1996@yahoo.com.hk>
2022-02-04 20:23:24 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,5 @@
-#!/usr/bin/env sh
-
-# abort on errors
-set -e
-
-# build
+npm ci
 npm run build
-
-# navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
+git add dist -f
 git commit -m "deploy"
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chankakit1996/count-down-timer.git
-
-cd -
+git subtree push --prefix dist master gh-pages
